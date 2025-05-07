3e2fe029bc1d367ac94c8c8856c0040f263b9c9b
mrboombastik_31 <timchik.oleksandr@gmail.com>
2025-05-07 20:14:25 +0300

    default commit (for private usage)

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '8026180920:AAGr14M4kDiox6PZFapI8wBdsWiNgX2Bw9s'
+BOT_NAME = '@BigBrotherNULP_bot'
+CHAT_ID = '-1002298975352'
+
+CHAT_ADMINISTRATORS = {}
+
+
+ban_words = ["заработок", "рублей", "ссылка", "пишіть сюди", "лну"]
+
+
+MUTED_GROUPS = [CHAT_ID]
